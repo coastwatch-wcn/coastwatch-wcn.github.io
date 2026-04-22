f577ce095dff4fcb86521b14cab883058de8d413
Madison Richardson <madirichardson23@gmail.com>
2026-04-22 16:16:05 -0700

    Try fixing js again

--- projects/turtlewatch/js/turtlewatch-nav.js
+++ projects/turtlewatch/js/turtlewatch-nav.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll(".project-tab");
-
-  // Get current page filename
   let current = window.location.pathname.split("/").pop();
 
-  // Handle empty path (homepage)
   if (!current) {
     current = "turtlewatch.html";
   }
 
   links.forEach(link => {
     let href = link.getAttribute("href");
-
-    // Convert .qmd → .html
     href = href.replace(".qmd", ".html");
 
-    // Match current page
-    if (current === href) {
+    if (href === current) {
       link.classList.add("active");
     }
   });
